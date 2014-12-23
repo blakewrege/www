03efcbf759dc93e207386ffc75626e61c719bc27
blake <Blakewrege1@gmail.com>
2014-12-23 04:07:24 -0500

    more css

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 </head>
 
 <div class="box" id="menu">
-	        	<span class="menu_item" id="menu_login_user"><a href="http://ccowmu.org/~gigglesbw4/oldindex.php">oldpage</a></span>
-	        	<span class="menu_item" id="menu_login_user"><a href="ccowmu/../.."> Homepage</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="oldindex.php">Old Webcam Page</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="ccowmu/../..">Ccowmu Homepage</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="http://t4ls.duckdns.org:8080/todoapp/html/">To Do App</a></span>
 	        </div>
 
 <h1>work in progress</h1>
