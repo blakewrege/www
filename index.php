3185e6213addaaafaecb69a1b0032c67341eae9a
blake <Blakewrege1@gmail.com>
2014-12-23 03:44:56 -0500

    style stuff

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
-work in progress
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+
+<p>work in progress</p>
 <a href="http://ccowmu.org/~gigglesbw4/oldindex.php">oldpage</a>
