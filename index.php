81950940fc5212413075554569bf20198fb3e5bf
blake <Blakewrege1@gmail.com>
2014-12-23 04:20:30 -0500

    added heading

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <head>
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
+<div id="header">
+<h1>UglyDuck Webserver</h1>
+</div>
 
 <div class="box" id="menu">
 	        	<span class="menu_item" id="menu_login_user"><a href="oldindex.php">Old Webcam Page</a></span>
