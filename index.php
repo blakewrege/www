fa6bd9cf86145db992150b5b7b9a85f24e696911
blake <Blakewrege1@gmail.com>
2014-12-23 04:02:56 -0500

    pretty menu

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 </head>
 
 <div class="box" id="menu">
-	        	<span class="menu_item" id="menu_login_user"><a href="createtask.php">Create New Task</a></span>
-	        	<span class="menu_item" id="menu_login_user"><a href="newtask/../.."> Homepage</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="http://ccowmu.org/~gigglesbw4/oldindex.php">oldpage</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="ccowmu/../.."> Homepage</a></span>
 	        </div>
 
 <h1>work in progress</h1>
-<a href="http://ccowmu.org/~gigglesbw4/oldindex.php">oldpage</a>
+
 
 
