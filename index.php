f1380362d0510face7604a944f1e8e2f54f149b4
blake <Blakewrege1@gmail.com>
2014-12-23 03:56:48 -0500

    test

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 
+<div class="box" id="menu">
+	        	<span class="menu_item" id="menu_login_user"><a href="createtask.php">Create New Task</a></span>
+	        	<span class="menu_item" id="menu_login_user"><a href="newtask/../.."> Homepage</a></span>
+	        </div>
+
 <h1>work in progress</h1>
 <a href="http://ccowmu.org/~gigglesbw4/oldindex.php">oldpage</a>
+
+
