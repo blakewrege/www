f44338280d8e823300fbb9191d6c12cd076fd034
Blake Wrege <blakewrege1@gmail.com>
2015-01-09 12:51:55 -0500

    added new webcam page link worked on about page

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
+<html>
+<style>
+body {
+    background-image: url("funnyduck.jpg");
+    background-position: 0px 0px;
+    background-repeat: no-repeat;
+    background-size: cover; 
+}
+</style>
+
+
 <head>
 <TITLE>Uglyduck Webserver</TITLE>
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
-<div id="header">
-<h1>UglyDuck Webserver</h1>
+<body>
+<div id="header" style="color:white">
+<h1><a style="color:white;text-decoration: none;"  href="index.php">UglyDuck Webserver</a></h1>
 </div>
 
 <div class="box" id="menu">
 	        	<span class="menu_item" id="menu1"><a href="oldindex.php">Old Webcam Page</a></span>
-	        	<span class="menu_item" id="menu2"><a href="ccowmu/../..">Ccowmu Homepage</a></span>
-	        	<span class="menu_item" id="menu3"><a href="http://t4ls.duckdns.org:8080/todoapp/html/">To Do App</a></span>
-	        	<span class="menu_item" id="menu4"><a href="testing/">Testing</a></span>
-	        	<span class="menu_item" id="menu5"><a href="Blake.php">About</a></span>
+			<span class="menu_item" id="menu2"><a href="webcam/">New Webcam Page</a></span>
+	        	<span class="menu_item" id="menu3"><a href="ccowmu/../..">Ccowmu Homepage</a></span>
+	        	<span class="menu_item" id="menu4"><a href="http://t4ls.duckdns.org:8080/todoapp/html/">To Do App</a></span>
+	        	<span class="menu_item" id="menu5"><a href="testing/">Testing</a></span>
+	        	<span class="menu_item" id="menu6"><a href="Blake.php">About</a></span>
 	        </div>
 <h2>work in progress</h2>
-
+</body></html>
 
 
